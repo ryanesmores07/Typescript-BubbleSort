f9b86f110d91b2099bbc5d869330591866c807b9
Ryan Esmores <ryanesmores07@gmail.com>
2023-03-07 14:46:37 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      // Last i elements are already in place
+      for (let j = 0; j < length - i - 1; j++) {
+        // Checking if the item at present iteration
+        // is greater than the next iteration
+        if (this.collection[j] > this.collection[j + 1]) {
+          // If the condition is true then swap them
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+    console.log(this.collection);
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+
+sorter.sort();
