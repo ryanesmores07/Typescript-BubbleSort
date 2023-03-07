2c6eb00488c09fe58418c8ed0a006ad6750be28f
Ryan Esmores <ryanesmores07@gmail.com>
2023-03-07 23:53:16 +0900

    added sorting for string

--- build/index.js
+++ build/index.js
@@ -1,25 +1,11 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            // Last i elements are already in place
-            for (let j = 0; j < length - i - 1; j++) {
-                // Checking if the item at present iteration
-                // is greater than the next iteration
-                if (this.collection[j] > this.collection[j + 1]) {
-                    // If the condition is true then swap them
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-        console.log(this.collection);
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection_1.CharactersCollection("XaabY");
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
+// console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      // Last i elements are already in place
-      for (let j = 0; j < length - i - 1; j++) {
-        // Checking if the item at present iteration
-        // is greater than the next iteration
-        if (this.collection[j] > this.collection[j + 1]) {
-          // If the condition is true then swap them
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-    console.log(this.collection);
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection("XaabY");
+const sorter = new Sorter(charactersCollection);
 
 sorter.sort();
+// console.log(numbersCollection.data);
+console.log(charactersCollection.data);
