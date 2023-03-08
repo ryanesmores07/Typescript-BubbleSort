1f46c3840a12d090787000d8fce6c895be421e00
Ryan Esmores <ryanesmores07@gmail.com>
2023-03-08 22:27:55 +0900

    finished Sorting function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const charactersCollection = new CharactersCollection("XaabY");
-const sorter = new Sorter(charactersCollection);
-
-sorter.sort();
-// console.log(numbersCollection.data);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+numbersCollection.sort();
+charactersCollection.sort();
+linkedList.sort();
+console.log(numbersCollection.data);
 console.log(charactersCollection.data);
+linkedList.print();
